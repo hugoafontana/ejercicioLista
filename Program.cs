3c6088b7b7e13c8f8b43727a6745aa60ab30de64
Hugo Ali Fontana <hugo.ali98@gmail.com>
2019-05-30 12:44:47 -0300

    Sube ejercicio 1 de listas resuelto

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] lista = new int[10] {2, 5, 7, 8, 10, 21, 12, 9 , 11, 5};
+            int suma = 0;
+            for(int i = 0; i < 10; i++){
+                suma += lista[i];
+            }
+            if(suma % 3 == 0)
+            {
+                Console.WriteLine("La suma es divisible por 3");
+            }
+            else
+            {
+                Console.WriteLine("La suma no es divisible por 3");
+            }
+            
+            Console.WriteLine(suma);
+        }
+    }
+}
